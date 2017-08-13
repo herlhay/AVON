a119beefd3b26caedf38162c88b9db620163e947
vicbob <30969522+vicbob@users.noreply.github.com>
2017-08-13 13:42:31 +0100

    Add files via upload

--- SearchFunction/SearchFunction/MainWindow.xaml.cs
+++ SearchFunction/SearchFunction/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
 namespace SearchFunction
 {
     /// <summary>
@@ -20,19 +20,25 @@ namespace SearchFunction
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+        static string pth = @"C:\Users\User\AppData\Local\Searches\";
+
         public MainWindow()
         {
             InitializeComponent();
+            btnSrch.IsEnabled = false;
+            lstBxAutoCmplt.Visibility = Visibility.Collapsed;
+            //lstBxAutoCmplt.ItemsSource = new List<string>() { "gg   K", "gh" };
         }
 
         private void txtBxSrch_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            
+
         }
 
         private void txtBxSrch_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-           
+
         }
 
         private void txtBxSrch_TextInput(object sender, TextCompositionEventArgs e)
@@ -56,6 +62,47 @@ namespace SearchFunction
             if (txtBxSrch.Text == "")
             {
                 txtBxSrch.Text = "Type your search here...";
+            }
+        }
+
+        private void btnSrch_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtBxSrch.Text != "Type your search here...")
+            {
+                Directory.CreateDirectory(pth);
+                using (StreamWriter swr = File.AppendText(pth + "hstryQry.stext"))
+                {
+                    swr.WriteLine(txtBxSrch.Text);
+                }
+            }
+        }
+
+        private void txtBxSrch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string[] qryLst = File.ReadAllLines(pth + "hstryQry.txt");
+            string qryTxt = txtBxSrch.Text;
+            List<string> autoLst = new List<string>();
+            autoLst.Clear();
+            foreach (var qry in qryLst)
+            {
+                if (!string.IsNullOrEmpty(txtBxSrch.Text) && txtBxSrch.Text != "Type your search here...")
+                {
+                    if (qry.ToLower().StartsWith(qryTxt.ToLower()))
+                    {
+                        autoLst.Add(qry);
+                    }
+                }
+
+            }
+            if (autoLst.Count > 0)
+            {
+                lstBxAutoCmplt.ItemsSource = autoLst;
+                lstBxAutoCmplt.Visibility = Visibility.Visible;
+            }
+            else if (txtBxSrch.Text.Equals(""))
+            {
+                lstBxAutoCmplt.Visibility = Visibility.Collapsed;
+                lstBxAutoCmplt.ItemsSource = null;
             }
         }
     }
